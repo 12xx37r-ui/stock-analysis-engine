c56d97789b6d636a5e6ef7620033ffd5bbf6dcf6
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 20:20:49 +0900

    Create price.py

---
+++ collectors/price.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def get_price(company_code):
+
+    # 임시 구조
+    # 이후 KIS API 연결
+
+    return {
+
+        "현재가": 0,
+
+        "시가총액": 0,
+
+        "PER": 0,
+
+        "PBR": 0,
+
+        "PSR": 0,
+
+        "EPS": 0,
+
+        "BPS": 0
+
+    }
