156b88f6ed25e8d0db8bbf56f3f92711e48278fa
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 20:48:01 +0900

    Update market.py

--- collectors/market.py
+++ collectors/market.py
@@ -1,22 +1,10 @@
+from collectors.kis import get_stock_price
+
+
+
 def get_market_data(stock_code):
 
-    return {
 
-        "현재가": 0,
-
-        "시가총액": 0,
-
-        "거래량": 0,
-
-        "PER": 0,
-
-        "PBR": 0,
-
-        "EPS": 0,
-
-        "BPS": 0,
-
-        "데이터출처":
-        "KIS API 연결 예정"
-
-    }
+    return get_stock_price(
+        stock_code
+    )
