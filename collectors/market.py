a9ef97f0969475b0a4107d2526fb752268366919
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 20:53:47 +0900

    Update market.py

--- collectors/market.py
+++ collectors/market.py
@@ -5,6 +5,92 @@ from collectors.kis import get_stock_price
 def get_market_data(stock_code):
 
 
-    return get_stock_price(
+    data = get_stock_price(
         stock_code
     )
+
+
+    output = data.get(
+        "원본",
+        {}
+    )
+
+
+    return {
+
+
+        "현재가":
+
+        float(
+            output.get(
+                "stck_prpr",
+                0
+            )
+        ),
+
+
+        "전일대비":
+
+        float(
+            output.get(
+                "prdy_vrss",
+                0
+            )
+        ),
+
+
+        "등락률":
+
+        float(
+            output.get(
+                "prdy_ctrt",
+                0
+            )
+        ),
+
+
+        "거래량":
+
+        int(
+            output.get(
+                "acml_vol",
+                0
+            )
+        ),
+
+
+        "시가":
+
+        float(
+            output.get(
+                "stck_oprc",
+                0
+            )
+        ),
+
+
+        "고가":
+
+        float(
+            output.get(
+                "stck_hgpr",
+                0
+            )
+        ),
+
+
+        "저가":
+
+        float(
+            output.get(
+                "stck_lwpr",
+                0
+            )
+        ),
+
+
+        "데이터출처":
+
+        "한국투자증권 KIS"
+
+    }
