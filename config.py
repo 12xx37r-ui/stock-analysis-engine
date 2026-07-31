a3d9ad2490d89bcc46be10fe56fd895da779e361
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 20:47:23 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,21 @@
 import os
 
-DART_API_KEY=os.getenv(
+
+DART_API_KEY = os.getenv(
     "DART_API_KEY"
+)
+
+
+KIS_APP_KEY = os.getenv(
+    "KIS_APP_KEY"
+)
+
+
+KIS_APP_SECRET = os.getenv(
+    "KIS_APP_SECRET"
+)
+
+
+KIS_BASE_URL = (
+    "https://openapi.koreainvestment.com:9443"
 )
