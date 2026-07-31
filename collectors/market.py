79154b407b12b501ef37dc984c95076415adbf32
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 21:22:56 +0900

    Update market.py

--- collectors/market.py
+++ collectors/market.py
@@ -1,153 +1,79 @@
-from collectors.kis import get_stock_price
+from collectors.kis import (
+    get_stock_price,
+    get_investor_trade
+)
 
 
 
 def get_market_data(stock_code):
 
 
-    data = get_stock_price(
+    price=get_stock_price(
         stock_code
     )
 
+
+    investor=get_investor_trade(
+        stock_code
+    )
 
 
     return {
 
 
         "현재가":
-
-        float(
-            data.get(
-                "stck_prpr",
-                0
-            )
-        ),
-
+        float(price.get("stck_prpr",0)),
 
 
         "전일대비":
-
-        float(
-            data.get(
-                "prdy_vrss",
-                0
-            )
-        ),
-
+        float(price.get("prdy_vrss",0)),
 
 
         "등락률":
-
-        float(
-            data.get(
-                "prdy_ctrt",
-                0
-            )
-        ),
-
+        float(price.get("prdy_ctrt",0)),
 
 
         "거래량":
-
-        int(
-            data.get(
-                "acml_vol",
-                0
-            )
-        ),
-
+        int(price.get("acml_vol",0)),
 
 
         "시가":
-
-        float(
-            data.get(
-                "stck_oprc",
-                0
-            )
-        ),
-
+        float(price.get("stck_oprc",0)),
 
 
         "고가":
-
-        float(
-            data.get(
-                "stck_hgpr",
-                0
-            )
-        ),
-
+        float(price.get("stck_hgpr",0)),
 
 
         "저가":
-
-        float(
-            data.get(
-                "stck_lwpr",
-                0
-            )
-        ),
-
+        float(price.get("stck_lwpr",0)),
 
 
         "PER":
-
-        float(
-            data.get(
-                "per",
-                0
-            )
-        ),
-
+        float(price.get("per",0)),
 
 
         "PBR":
-
-        float(
-            data.get(
-                "pbr",
-                0
-            )
-        ),
-
+        float(price.get("pbr",0)),
 
 
         "EPS":
-
-        float(
-            data.get(
-                "eps",
-                0
-            )
-        ),
-
+        float(price.get("eps",0)),
 
 
         "BPS":
-
-        float(
-            data.get(
-                "bps",
-                0
-            )
-        ),
-
+        float(price.get("bps",0)),
 
 
         "시가총액":
+        float(price.get("hts_avls",0)),
 
-        float(
-            data.get(
-                "hts_avls",
-                0
-            )
-        ),
 
+        "수급":
+        investor,
 
 
         "데이터출처":
-
         "한국투자증권 KIS"
 
     }
