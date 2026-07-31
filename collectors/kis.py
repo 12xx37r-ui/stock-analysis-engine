facf9240b5fb33cd8f85afdd1404cc4c670135d3
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 21:30:30 +0900

    Update kis.py

--- collectors/kis.py
+++ collectors/kis.py
@@ -1,3 +1,109 @@
+import requests
+
+from config import (
+    KIS_APP_KEY,
+    KIS_APP_SECRET,
+    KIS_BASE_URL
+)
+
+
+def get_access_token():
+
+    url = f"{KIS_BASE_URL}/oauth2/tokenP"
+
+    body = {
+        "grant_type": "client_credentials",
+        "appkey": KIS_APP_KEY,
+        "appsecret": KIS_APP_SECRET
+    }
+
+    response = requests.post(
+        url,
+        json=body,
+        timeout=10
+    )
+
+    data = response.json()
+
+    return data.get(
+        "access_token"
+    )
+
+
+
+def kis_request(tr_id, path, params):
+
+    token = get_access_token()
+
+    if not token:
+        return {}
+
+    headers = {
+
+        "authorization":
+        "Bearer " + token,
+
+        "appkey":
+        KIS_APP_KEY,
+
+        "appsecret":
+        KIS_APP_SECRET,
+
+        "tr_id":
+        tr_id,
+
+        "custtype":
+        "P"
+
+    }
+
+
+    response = requests.get(
+
+        KIS_BASE_URL + path,
+
+        headers=headers,
+
+        params=params,
+
+        timeout=10
+
+    )
+
+
+    return response.json()
+
+
+
+def get_stock_price(stock_code):
+
+
+    data = kis_request(
+
+        "FHKST01010100",
+
+        "/uapi/domestic-stock/v1/quotations/inquire-price",
+
+        {
+
+            "FID_COND_MRKT_DIV_CODE":
+            "J",
+
+            "FID_INPUT_ISCD":
+            stock_code
+
+        }
+
+    )
+
+
+    return data.get(
+        "output",
+        {}
+    )
+
+
+
 def get_investor_trade(stock_code):
 
 
@@ -9,9 +115,11 @@ def get_investor_trade(stock_code):
 
         {
 
-            "FID_COND_MRKT_DIV_CODE": "J",
+            "FID_COND_MRKT_DIV_CODE":
+            "J",
 
-            "FID_INPUT_ISCD": stock_code
+            "FID_INPUT_ISCD":
+            stock_code
 
         }
 
@@ -22,7 +130,6 @@ def get_investor_trade(stock_code):
     print(data)
 
 
-
     output = data.get(
         "output",
         []
@@ -30,6 +137,7 @@ def get_investor_trade(stock_code):
 
 
     if not output:
+
         return {
 
             "외국인순매수":0,
@@ -45,6 +153,7 @@ def get_investor_trade(stock_code):
 
 
     return {
+
 
         "외국인순매수":
         float(
