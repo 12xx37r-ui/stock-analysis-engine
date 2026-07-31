3394ed38b57a4149ff18dbbb7a5a5ea3f7dc35d9
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 20:22:13 +0900

    Update price.py

--- collectors/price.py
+++ collectors/price.py
@@ -3,23 +3,40 @@ import requests
 
 def get_price(company_code):
 
-    # 임시 구조
-    # 이후 KIS API 연결
 
-    return {
+    # 현재는 테스트용
+    # 다음 단계에서 KIS API 연결
 
-        "현재가": 0,
 
-        "시가총액": 0,
+    price_data = {
 
-        "PER": 0,
 
-        "PBR": 0,
+        "현재가":0,
 
-        "PSR": 0,
 
-        "EPS": 0,
+        "시가총액":0,
 
-        "BPS": 0
+
+        "PER":0,
+
+
+        "PBR":0,
+
+
+        "PSR":0,
+
+
+        "EPS":0,
+
+
+        "BPS":0,
+
+
+        "데이터출처":
+        "KIS API 연결 전"
+
 
     }
+
+
+    return price_data
