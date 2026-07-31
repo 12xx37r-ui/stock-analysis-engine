1662e7b48cccad8b625f1c25982a14fd5f2ff5f7
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 19:43:26 +0900

    Create financial.py

---
+++ analyzers/financial.py
@@ -0,0 +1,16 @@
+def analyze_financial(company):
+
+    return {
+
+        "company": company,
+
+        "roe": None,
+        "revenue_growth": None,
+        "operating_growth": None,
+        "net_income_growth": None,
+
+        "debt_ratio": None,
+
+        "status": "데이터 연결 준비중"
+
+    }
