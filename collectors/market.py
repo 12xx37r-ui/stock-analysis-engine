ebcbc4f77b03751708dd9318201261220adcd32c
12xx37r-ui <12xx37r@gmail.com>
2026-07-31 20:57:20 +0900

    Update market.py

--- collectors/market.py
+++ collectors/market.py
@@ -1,19 +1,22 @@
-from collectors.kis import get_stock_price
+from collectors.kis import (
+    get_stock_price,
+    get_stock_finance
+)
 
 
 
 def get_market_data(stock_code):
 
 
-    data = get_stock_price(
+    price=get_stock_price(
         stock_code
     )
 
 
-    output = data.get(
-        "원본",
-        {}
+    finance=get_stock_finance(
+        stock_code
     )
+
 
 
     return {
@@ -22,71 +25,133 @@ def get_market_data(stock_code):
         "현재가":
 
         float(
-            output.get(
+            price.get(
                 "stck_prpr",
                 0
             )
         ),
 
 
+
         "전일대비":
 
         float(
-            output.get(
+            price.get(
                 "prdy_vrss",
                 0
             )
         ),
 
 
+
         "등락률":
 
         float(
-            output.get(
+            price.get(
                 "prdy_ctrt",
                 0
             )
         ),
 
 
+
         "거래량":
 
         int(
-            output.get(
+            price.get(
                 "acml_vol",
                 0
             )
         ),
 
 
+
         "시가":
 
         float(
-            output.get(
+            price.get(
                 "stck_oprc",
                 0
             )
         ),
 
 
+
         "고가":
 
         float(
-            output.get(
+            price.get(
                 "stck_hgpr",
                 0
             )
         ),
 
 
+
         "저가":
 
         float(
-            output.get(
+            price.get(
                 "stck_lwpr",
                 0
             )
         ),
+
+
+
+        "PER":
+
+        float(
+            finance.get(
+                "per",
+                0
+            )
+        ),
+
+
+
+        "PBR":
+
+        float(
+            finance.get(
+                "pbr",
+                0
+            )
+        ),
+
+
+
+        "EPS":
+
+        float(
+            finance.get(
+                "eps",
+                0
+            )
+        ),
+
+
+
+        "BPS":
+
+        float(
+            finance.get(
+                "bps",
+                0
+            )
+        ),
+
+
+
+        "시가총액":
+
+        float(
+            finance.get(
+                "stck_avls",
+                0
+            )
+        ),
+
 
 
         "데이터출처":
